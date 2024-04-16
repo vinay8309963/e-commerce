ef7a05952697e9870194b01aea476b28c2a743f5
Vinay <vinay8309963@gmail.com>
2024-04-16 17:22:24 +0530

    e-commerce website

---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>E-commerce has helped businesses (especially those with a narrow reach like small businesses) gain access to and establish a wider market presence by providing cheaper and more efficient distribution channels for their products or services. Target (TGT) supplemented its brick-and-mortar presence with an online store that allows customers to purchase everything from clothes and coffeemakers to toothpaste and action figures right from their homes.</p>
+        <p>
+        E-commerce has helped businesses (especially those with a narrow reach like small businesses) gain access to and establish a wider market presence by providing cheaper and more efficient distribution channels for their products or services. 
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
